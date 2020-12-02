08d0eb700b5a36c338c60646fb58dd2b2ea99c43
Helena Magaldi <helenamagaldi@gmail.com>
2020-12-02 10:52:56 -0300

    starting 'Errors' folder

---
+++ 2. Operators/3comparisonOperators.py
@@ -0,0 +1,44 @@
+# (base) helenamagaldi@MacBook-Pro-de-Helena-2 in pythonbootcamp
+# $ python3
+# Python 3.8.3 (default, Jul  2 2020, 11:26:31) 
+# [Clang 10.0.0 ] :: Anaconda, Inc. on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> x = 3
+# >>> y = 3
+# >>> z = 5
+# >>> x
+# 3
+# >>> y
+# 3
+# >>> z
+# 5
+# >>> x == y
+# True
+# >>> x == 5
+# False
+# >>> x == z
+# False
+# >>> x != y
+# False
+# >>> x != z
+# True
+# >>> x > 5
+# False
+# >>> x < z
+# True
+# >>> x > z
+# False
+# >>>  x <= y
+#   File "<stdin>", line 1
+#     x <= y
+#     ^
+# IndentationError: unexpected indent
+# >>> x <= y
+# True
+# >>> x >= z
+# False
+# >>> 3 <= 9
+# True
+# >>> 9 >= 20
+# False
+# >>> 
